c6e3b32ed2d165f3006e8f10faa23b78073423e9
Fabian Gutierrez <fega.hg@gmail.com>
2021-05-28 20:43:39 -0500

    added dist folder

---
+++ dist/js/about.e02e7bee.js
@@ -0,0 +1,6 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([['about'], {
+  f820(n, t, a) {
+    a.r(t); const e = a('7a23'); const o = { class: 'about' }; const c = Object(e.f)('h1', null, 'This is an about page', -1); function s(n, t) { return Object(e.o)(), Object(e.d)('div', o, [c]); } const u = {}; u.render = s; t.default = u;
+  },
+}]);
+// # sourceMappingURL=about.e02e7bee.js.map
